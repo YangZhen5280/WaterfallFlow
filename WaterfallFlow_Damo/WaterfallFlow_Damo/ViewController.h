b7e0a431958c98c64126fde3170a3c3a05fad58a
yyzz <2397jksjdl@.163.com>
2015-11-02 08:31:59 +0800

    初始化项目

---
+++ WaterfallFlow_Damo/WaterfallFlow_Damo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaterfallFlow_Damo
+//
+//  Created by 杨振 on 15/11/2.
+//  Copyright © 2015年 杨振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WaterfallFlow_Damo/WaterfallFlow_Damo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WaterfallFlow_Damo
+//
+//  Created by 杨振 on 15/11/2.
+//  Copyright © 2015年 杨振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
