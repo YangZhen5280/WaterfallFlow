cabde8971c80e4bbd7513654e9da9c58ba6e42fd
yyzz <2397jksjdl@.163.com>
2015-11-02 13:37:44 +0800

    布局空间基础边距等信息

--- WaterfallFlow_Damo/WaterfallFlow_Damo/Classes/Controller/YYWaterfallFlowLayout.m
+++ WaterfallFlow_Damo/WaterfallFlow_Damo/Classes/Controller/YYWaterfallFlowLayout.m
@@ -8,12 +8,43 @@
 
 #import "YYWaterfallFlowLayout.h"
 
+#define YYCollectionViewWidth self.collectionView.frame.size.width
+
+static const CGFloat YYDefaultRowMargin = 10;
+static const CGFloat YYDefaultColMargin = 20;
+static const UIEdgeInsets YYDefaultEdginsets = {10, 10, 10, 10};
+static const CGFloat YYDefaultColumnCount = 3;
+
+@interface YYWaterfallFlowLayout ()
+
+@property (nonatomic, strong) NSMutableArray *columnMaxYarray;
+
+@end
 @implementation YYWaterfallFlowLayout
+
+#pragma mark - lazy
+- (NSMutableArray *)columnMaxYarray {
+    if (!_columnMaxYarray) {
+        _columnMaxYarray = [[NSMutableArray alloc] init];
+    }
+    return _columnMaxYarray;
+}
+
+#pragma mark - 实现内部方法
+- (void)prepareLayout {
+    [super prepareLayout];
+    [self.columnMaxYarray removeAllObjects];
+    for (int i = 0; i< YYDefaultColumnCount; i ++) {
+        [self.columnMaxYarray addObject:@(0)];
+    }
+
+}
 
 /**
  *  说明所有元素最终显示出来的布局属性
  */
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    
     NSMutableArray *arrM = [NSMutableArray array];
     
     NSInteger count = [self.collectionView numberOfItemsInSection:0];
@@ -25,8 +56,13 @@
     }
     return arrM;
 }
+/**
+ *  说明indexPath位置cell的布局属性
+ */
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
     UICollectionViewLayoutAttributes *attrs = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+    
     
     attrs.center = CGPointMake(100, 100);
     attrs.size = CGSizeMake(100, 150);
